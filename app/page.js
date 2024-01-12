57d81400174dd38bb3f1ff54989f1b7a5f16cb20
Jeff Hartigan <jeff@eag.network>
2024-01-12 13:07:21 -0800

    added copyright

--- app/page.js
+++ app/page.js
@@ -9,15 +9,21 @@ export default function Home() {
   const eagLpAddress = '0x7f00995f977a8a64abd4b888e5cf09d86f91ca66';
 
   return (
+    <div>
     <main className="flex min-h-screen flex-col items-center pt-8">
       <div className="flex gap-20 font-mono">
         <CurrentLPStatus lpAddress={barcLpAddress} />
         <CurrentLPStatus lpAddress={eagLpAddress} />
       </div>
 
-      <div className='flex justify-center w-full my-4'>
+      <div className='flex justify-center w-full mt-4'>
         <TradeSimulation />
       </div>
+      <footer className='mt-4'>
+    <p className="text-center text-sm">© 2023 Hack The Planet. All rights reserved.</p>
+    </footer>
     </main>
+    
+    </div>
   )
 }
